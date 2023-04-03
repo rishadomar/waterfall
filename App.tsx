24a20b76e1a10b4996f5f43d1a26fb9bf51275ff
Rishad Omar <rishad@aux.co.za>
2023-04-03 21:53:46 +0200

    Add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 import { Story } from './story';
 import Pages from './components/Pages';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <Pages pages={Story.pages} availableTweets={Story.availableTweets} />
+            <StatusBar style='auto' />
         </View>
     );
 }
